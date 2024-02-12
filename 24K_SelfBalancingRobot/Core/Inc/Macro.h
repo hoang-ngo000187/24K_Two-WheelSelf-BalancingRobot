474c1d0b2fe7d9cb72b92ce8fe50d4d599a11584
hoang.ngo000187 <hoang.ngo000187@gmail.com>
2024-02-13 01:18:25 +0700

    [ADD] Read MPU6050 value, control step motor NEMA17

---
+++ 24K_SelfBalancingRobot/Core/Inc/Macro.h
@@ -0,0 +1,21 @@
+/****************************************************************************************************************************
+ * @author: Ngo Kien Hoang
+ * @email: hoang.ngo000187@gmail.com
+ *
+ * This file contains some macros which define for some C data types.
+ ****************************************************************************************************************************/
+#include "stdint.h"
+
+typedef int8_t		INT8;
+typedef uint8_t		UINT8;
+
+typedef int16_t		INT16;
+typedef uint16_t	UINT16;
+
+typedef int32_t		INT32;
+typedef uint32_t	UINT32;
+
+typedef int64_t		INT64;
+typedef uint64_t	UINT64;
+
+typedef float       FLOAT;
